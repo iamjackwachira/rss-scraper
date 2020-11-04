a8e26595d5c10f542793317dc4c83b8a3582adbf
Jack Wachira <iamjackwachira@gmail.com>
2020-11-04 20:49:52 +0300

    Add feed follow models

--- apps/feeds/models.py
+++ apps/feeds/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 from core.models import BaseModel
@@ -14,9 +15,8 @@ class BaseFeed(BaseModel):
 
 
 class Feed(BaseFeed):
-    """
-    RSS Feeds
-    """
+    """ RSS Feeds """
+
     language = models.CharField(max_length=50)
 
     class Meta:
@@ -27,13 +27,12 @@ class Feed(BaseFeed):
 
 
 class FeedItem(BaseFeed):
-    """
-    Feed Items contained in a Feed
-    """
+    """ Feed Items contained in a Feed """
 
     feed = models.ForeignKey(
         Feed, on_delete=models.CASCADE, related_name='feed_items')
     is_permalink = models.BooleanField(default=False)
+    read = models.BooleanField(default=False)
 
     class Meta:
         verbose_name = 'Feed Item'
@@ -41,3 +40,19 @@ class FeedItem(BaseFeed):
 
     def __str__(self):
         return f'{self.title}'
+
+
+class FeedFollow(BaseModel):
+    """ Feeds a user follows """
+
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='feed_follow')
+    feed = models.ForeignKey(
+        Feed, on_delete=models.CASCADE, related_name='feed_follow')
+
+    class Meta:
+        verbose_name = 'Feed Follow'
+        verbose_name_plural = 'Feed Follow'
+
+    def __str__(self):
+        return f'{self.user} - {self.feed}'
